f933aea0b59556f2a1270399b7367395286fa57c
shrued <shrutisara10@gmail.com>
2021-01-07 18:19:03 +0530

    appbar elevation

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -36,6 +36,7 @@ class _HomeState extends State<Home> {
       home: Scaffold(
         bottomNavigationBar: _light ? LightBottomNavBar() : DarkBottomNavBar(),
         appBar: AppBar(
+          elevation: 0,
           title: Text("Discover"),
         ),
         body: Center(
